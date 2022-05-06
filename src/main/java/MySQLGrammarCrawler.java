c709102c95f6b0fdd879f0a797f73da35d8a295e
Jason Fulghum <jason@dolthub.com>
2022-05-06 09:36:27 -0700

    Starting to better organize code that takes an abstract statement template and turns it into a valid statement that can be executed.

--- src/main/java/MySQLGrammarCrawler.java
+++ src/main/java/MySQLGrammarCrawler.java
@@ -61,7 +61,7 @@ public class MySQLGrammarCrawler {
 
             String s = statementPrefix + generatedTemplate.toString();
 
-            s = s.replaceFirst("foo", randomNewTableName());
+            s = s.replaceFirst("foo", StatementReifier.randomNewTableName());
             int i = 1;
             while (s.contains("foo")) {
                 s = s.replaceFirst("foo", "c" + i);
@@ -72,67 +72,68 @@ public class MySQLGrammarCrawler {
         }
     }
 
-    private static int tableNameCounter = 1;
+    public static class StatementReifier {
+        private static int tableNameCounter = 1;
 
-    public static String randomNewTableName() {
-        return "t" + Integer.toHexString(tableNameCounter++);
+        public static String randomNewTableName() {
+            return "t" + Integer.toHexString(tableNameCounter++);
+        }
+
+
+        public static String translateSymbol(String symbolName) {
+            // TODO: Load these from the lexer instead of manually defining them...
+            switch (symbolName) {
+                case "DOT_SYMBOL":
+                    return ".";
+                case "COMMA_SYMBOL":
+                    return ",";
+                case "AT_SIGN_SYMBOL":
+                    return "@";
+                case "EQUAL_OPERATOR":
+                    return "=";
+                case "SEMICOLON_SYMBOL":
+                    return ";";
+                case "OPEN_PAR_SYMBOL":
+                    return "(";
+                case "CLOSE_PAR_SYMBOL":
+                    return ")";
+                case "MULT_OPERATOR":
+                    return "*";
+                case "OPEN_CURLY_SYMBOL":
+                    return "{";
+                case "CLOSE_CURLY_SYMBOL":
+                    return "}";
+            }
+
+            // TODO: All of these identifiers need more granular control for customizing in statements
+            switch (symbolName) {
+                case "IDENTIFIER":
+                    return "foo";
+                case "BACK_TICK_QUOTED_ID":
+                    return "`foo`";
+                case "SINGLE_QUOTED_TEXT":
+                    return "'text'";
+                case "DOUBLE_QUOTED_TEXT":
+                    return "\"text\"";
+                case "INT_NUMBER":
+                    return "42";
+                case "DECIMAL_NUMBER":
+                    return "4.2";
+            }
+
+            if (symbolName.endsWith("_SYMBOL")) {
+                return symbolName.substring(0, symbolName.indexOf("_SYMBOL"));
+            }
+
+            return symbolName;
+        }
     }
-
-    public static String translateSymbol(String symbolName) {
-        // TODO: Load these from the lexer instead of manually defining them...
-        switch (symbolName) {
-            case "DOT_SYMBOL":
-                return ".";
-            case "COMMA_SYMBOL":
-                return ",";
-            case "AT_SIGN_SYMBOL":
-                return "@";
-            case "EQUAL_OPERATOR":
-                return "=";
-            case "SEMICOLON_SYMBOL":
-                return ";";
-            case "OPEN_PAR_SYMBOL":
-                return "(";
-            case "CLOSE_PAR_SYMBOL":
-                return ")";
-            case "MULT_OPERATOR":
-                return "*";
-            case "OPEN_CURLY_SYMBOL":
-                return "{";
-            case "CLOSE_CURLY_SYMBOL":
-                return "}";
-        }
-
-        // TODO: All of these identifiers need more granular control for customizing in statements
-        switch (symbolName) {
-            case "IDENTIFIER":
-                return "foo";
-            case "BACK_TICK_QUOTED_ID":
-                return "`foo`";
-            case "SINGLE_QUOTED_TEXT":
-                return "'text'";
-            case "DOUBLE_QUOTED_TEXT":
-                return "\"text\"";
-            case "INT_NUMBER":
-                return "42";
-            case "DECIMAL_NUMBER":
-                return "4.2";
-        }
-
-        if (symbolName.endsWith("_SYMBOL")) {
-            return symbolName.substring(0, symbolName.indexOf("_SYMBOL"));
-        }
-
-        return symbolName;
-    }
-
-    private static final boolean DEBUG = true;
 
     public static void processElement(CrawlContext currentContext) {
         Element element = currentContext.elementToProcess;
         TemplateBuffer generatedTemplate = currentContext.generatedTemplate;
 
-//        if (DEBUG) {
+//        if (true) {
 //            long heapSize = Runtime.getRuntime().totalMemory();
 //            long heapMaxSize = Runtime.getRuntime().maxMemory();
 //            System.out.println("Max Heap: " + heapMaxSize);

ea9f91ce3452e8e0480498afaa74198c7b04eb22
Jason Fulghum <jason@dolthub.com>
2022-06-07 15:48:27 -0700

    Adding copyright headers

--- src/main/java/StatementReifier.java
+++ src/main/java/StatementReifier.java
@@ -1,3 +1,17 @@
+// Copyright 2022 Dolthub, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 public class StatementReifier {
     private static int tableNameCounter = 1;
     private static long textId = 0;
@@ -110,7 +124,7 @@ public class StatementReifier {
     private static String randomNewTableName() {
         return "t" + Integer.toHexString(tableNameCounter++);
     }
-    
+
     private static int randomInt(int min, int max) {
         return (int) Math.floor(Math.random() * (max - min + 1) + min);
     }
